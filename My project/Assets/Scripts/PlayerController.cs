df22dc9cacf3ab76c129ae4821958ef2af1b384f
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2022-11-09 14:51:45 +0800

    change speed

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 30.0f;
+    private float speed = 20.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
